84881211539ea22bed13fda1a5207504a561a065
LuizLins <luizmagao@gmail.com>
2026-03-03 21:16:04 -0300

    Classe completa de médico

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
-
 class Medico {
+    function __construct(
+        private string $crm, 
+        private string $nome, 
+        private string $especialidade
+        ) {}
 
-    private string $crm_medico;
-    private string $nome_medico;
-    private string $especialidade;
-
-    function __construct(string $crm, string $nome, string $especialidade) {
-        $this->crm_medico = $crm;
-        $this->nome_medico = $nome;
-        $this->especialidade = $especialidade;
+    public function mostrarMedico() {
+        echo "CRM: " .  $this->crm . PHP_EOL;
+        echo "Nome: ". $this->nome . PHP_EOL;
+        echo "Especialidade: " . $this->especialidade . PHP_EOL;
     }
-
 }
+$medico = new Medico("CRM/PI 112469", "Luiz Lins", "Urologista");
+$medico->mostrarMedico();
